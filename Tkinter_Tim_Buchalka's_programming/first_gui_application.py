fcdda497e694361e84a46d48331b7958017b60a8
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-10-23 20:15:20 -0300

    Create first_gui_application.py

---
+++ Tkinter_Tim_Buchalka's_programming/first_gui_application.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+root.geometry('500x450')
+
+button1 = Button(root, text='Click me')
+button2 = Button(root, text='Click me')
+button1.pack()
+button2.pack()
+
+root.mainloop()
