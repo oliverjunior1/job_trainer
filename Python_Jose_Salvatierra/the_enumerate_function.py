972ad4b4cea267ed13cb7d46bd13b6b9425afe16
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-10-23 21:11:57 -0300

    Create the_enumerate_function.py

---
+++ Python_Jose_Salvatierra/the_enumerate_function.py
@@ -0,0 +1,17 @@
+friends = ["Ralf", "John", "Anna"]
+
+counter = 0
+
+# for friend in friends:
+#     print(counter)
+#     print(friend)
+#     counter = counter + 1
+
+for counter, friend in enumerate(friends, start=1):
+    print(counter)
+    print(friend)
+
+print(list(enumerate(friends)))
+print(dict(enumerate(friends)))
+
+# print(list(zip([0,1,2],friends)))
