818ac85dbf7b95ce64575b6003c0d076ac08016c
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-10-24 23:17:45 -0300

    Create main_game_loop.py

---
+++ pygame_penny_de_byll/main_game_loop.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1920,1080),0,32)
+pygame.display.set_caption("Hello Pygame")
+screen.fill((0,0,0))
+
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+pygame.quit()
