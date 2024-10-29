3dc873cbe2f1f196aa25492dc34c6ecc73dff7c6
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-10-29 20:44:55 -0300

    Update object_oriented_project.py

---
+++ Python_Jose_Salvatierra/object_oriented_project.py
@@ -0,0 +1,32 @@
+from numpy.ma.extras import average
+
+my_student = {
+    'name':'Ralf Smith',
+    'grades':[70,88,90,99]
+}
+
+def average_grade(student):
+    return sum(student['grades'])/len(student['grades'])
+
+print(average_grade(my_student))
+
+class Student:
+    def __init__(self, new_name, new_grade):
+        self.name = new_name
+        self.grade = new_grade
+
+    def average(self):
+        return sum(self.grade)/len(self.grade)
+    
+    def __str__(self):
+        return f'The name of the student is {self.name}, the grade is: {self.average()}'
+
+student_one = Student("Ralf Smith", [70,88,90,99])
+student_two = Student("John Claud", [70,90,55,96])
+
+# print(student_one.name)
+# print(student_one.average())
+#
+# print(student_two.average())
+print(student_one)
+print(student_two)
