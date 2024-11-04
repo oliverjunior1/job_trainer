185c7cb3abf567bd22ebca227e44f4c0cde63af9
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-11-04 20:54:12 -0300

    Update app.py

--- Python_Jose_Salvatierra/files/app.py
+++ Python_Jose_Salvatierra/files/app.py
@@ -7,5 +7,7 @@ print(file_content)
 
 user_name = input('Enter your name: ')
 
-my_file_writing = open('data.txt', 'w')
+my_file_writing = open('data_name.txt', 'w')
+my_file_writing.write(user_name)
 my_file_writing.close()
+
