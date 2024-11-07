ef037d70f46ebb050827a4bb6cdbc66cc75b3b67
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-11-07 19:27:18 -0300

    Create radio_buttons.py

---
+++ Tkinter_Tim_Buchalka's_programming/radio_buttons.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+def callback():
+    print('Your name:', entry.get())
+    print('Your password:'+entry2.get())
+    if chvar.get()==1:
+        print('Remember me selected')
+    else:
+        print('not selected')
+    print(gender.get())
+
+entry = ttk.Entry(root, width=30)
+entry2 = ttk.Entry(root, width=30)
+entry.insert(0,'Please enter your name')
+entry2.insert(0,'Please enter your password')
+button = ttk.Button(root, text='Enter')
+lbltitle = ttk.Label(text='Our Title Here', font=(('Arial'),22))
+lblname = ttk.Label(text='Your Name:')
+lblpass = ttk.Label(text='Your Password:')
+lbltitle.grid(row=0, column=0, columnspan=2)
+lblname.grid(row=1, column=0, sticky=W)
+lblpass.grid(row=2, column=0)
+entry.grid(row=1, column=1)
+entry2.grid(row=2, column=1)
+button.grid(row=3, column=1, sticky=E, pady=5)
+chvar=IntVar()
+chvar.set(0)
+cbox = Checkbutton(root, text='Remember me', variable=chvar, font='Arial 16').grid(row=4, column=0, sticky=E, columnspan=2)
+button.config(command=callback)
+
+gender = StringVar()
+ttk.Radiobutton(root, text='male', value='male', var=gender).grid(row=5, column=0)
+ttk.Radiobutton(root, text='female', value='female', var=gender).grid(row=5, column=1)
+root.geometry('500x450')
+root.mainloop()
