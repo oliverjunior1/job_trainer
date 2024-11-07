0af0d01a9ca447c8b27f565ea5bc5659252937b6
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-11-07 19:57:50 -0300

    Create csv_read.py

---
+++ Python_Jose_Salvatierra/csv_read/csv_read.py
@@ -0,0 +1,17 @@
+file = open('csv_data.txt', 'r')
+lines = file.readlines()
+file.close()
+
+lines = [line.strip() for line in lines[1:]]
+
+for line in lines:
+    person_data= line.split(',')
+    name = person_data[0]
+    age = person_data[1]
+    university = person_data[2]
+    degree = person_data[3]
+
+    print(f'{name.title()} is {age}, studying {degree.capitalize()} at {university.title()}.')
+
+sample_csv_value = ','.join(['Ralf', '25', 'MIT', 'Computer Science'])
+print(sample_csv_value)
