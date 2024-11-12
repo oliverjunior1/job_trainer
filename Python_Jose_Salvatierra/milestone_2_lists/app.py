7fa92d5f0061f410ac3df3924365ccb773b3b1cd
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-11-12 16:06:00 -0300

    Update app.py

--- Python_Jose_Salvatierra/milestone_2_lists/app.py
+++ Python_Jose_Salvatierra/milestone_2_lists/app.py
@@ -1,3 +1,4 @@
+
 from utils import database
 
 USER_CHOICE = '''
@@ -14,10 +15,41 @@ Your choice:
 def menu():
     user_input = input(USER_CHOICE)
     while user_input != 'q':
-        pass
+        if user_input == 'a':
+            prompt_add_book()
+        elif user_input == 'l':
+            list_books()
+        elif user_input == 'r':
+            prompt_read_book()
+        elif user_input == 'd':
+            prompt_delete_book()
+        else:
+            print('Unknown command. Please try again.')
 
-# def prompt_add_book() ask for book name and author
-# def list_books() show all the books in our list
-# def prompt_read_book() ask for book name and change it to "read" in our list
-# def prompt_delete_book() ask for book name and remove book from list
+        user_input = input(USER_CHOICE)
 
+def prompt_add_book():
+    name = input('Enter the new name book:')
+    author = input('Enter the new book author:')
+
+def list_books():
+    books = database.get_all_books()
+    for book in books:
+        read = 'YES' if book['read'] else 'NO'
+        print(f"{book['name']} by {book['author']},read:{read}")
+
+def prompt_read_book():
+    name = input('Enter the name of the book you just finished reading:')
+
+    database.mark_book_as_read(name)
+
+def prompt_delete_book():
+    name = input('Enter the name of the book you wish to delete: ')
+
+    database.delete_book(name)
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] != name]
+
+menu()
