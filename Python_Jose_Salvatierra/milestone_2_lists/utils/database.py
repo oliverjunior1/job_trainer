0a8ea2f962729767be22fbf474e6834402e27143
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-11-12 16:06:09 -0300

    Update database.py

--- Python_Jose_Salvatierra/milestone_2_lists/utils/database.py
+++ Python_Jose_Salvatierra/milestone_2_lists/utils/database.py
@@ -6,3 +6,12 @@ books = []
 
 def add_book(name, author):
     books.append({'name':name, 'author': author, 'read': False})
+
+def get_all_books():
+    return books
+
+def mark_book_as_read(name):
+    for book in books:
+        if book['name']== name:
+            book['read'] = True
+
