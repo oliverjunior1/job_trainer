2c497181268b6a627119747a065b800e33ef0958
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2024-11-18 08:17:31 -0300

    Create text_editor.py

---
+++ Tkinter_Tim_Buchalka's_programming/text_editor.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+from pygame.examples.audiocapture import callback
+
+root= Tk()
+def callback():
+    result=textEditor.get(1.0, END)
+    print(result)
+textEditor = Text(root, width=30, height=10, font=(('Times'), 15), wrap=WORD)
+textEditor.grid(row=0, column=0, pady=20, padx=40)
+textEditor.insert(INSERT, 'Hello I am a tkinter widget')
+textEditor.config(state='disabled')
+textEditor.config(state='normal')
+button = Button(root, text='Save',width=10, height=1, command=callback)
+button.grid(row=3, column=0)
+root.geometry('550x550')
+root.mainloop()
