58eef9359a32e2428989e3f3ac4a21a079e4f795
oliverjunior1 <oliver-junior@hotmail.com>
2024-11-28 22:13:01 -0300

    Create teste.py

---
+++ DIO_course/formacao_python_fundamentals/teste.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+lista_de_compras = [
+    "pera",
+    "banana",
+    "carambola",
+    "melancia",
+    "ramara",
+    "melao",
+    "pera",
+    "uva",
+    "laranja",
+    "jaca",
+    "jilo",
+    "repolho",
+    "tomate",
+    "arroz",
+    "feijao",
+    "oleo",
+    "bolacha",
+    "macarrao",
+]
+
+
+print(lista_de_compras)
