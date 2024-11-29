edcadd0249a861dfbe9b90b486ee86e7d68dd532
oliverjunior1 <oliver-junior@hotmail.com>
2024-11-28 22:47:38 -0300

    Update BreakinBricks.py

--- pygame_penny_de_byll/BreakinBricks/main/BreakinBricks.py
+++ pygame_penny_de_byll/BreakinBricks/main/BreakinBricks.py
@@ -18,6 +18,17 @@ ball_rect = ball.get_rect()
 brick = pygame.image.load('../images/brick.png')
 brick = brick.convert_alpha()
 brick_rect = brick.get_rect()
+bricks = []
+brick_rows = 5
+brick_gap = 10
+brick_cols = screen.get_width()//(brick_rect[2] + brick_gap)
+side_gap = (screen.get_width() - (brick_rect[2] + brick_gap) * brick_cols + brick_gap) // 2
+
+for y in range(brick_rows):
+    brickY = y * (brick_rect[3]+brick_gap)
+    for x in range(brick_cols):
+        brickX = x * (brick_rect[2]+brick_gap) + side_gap
+        bricks.append((brickX, brickY))
 
 clock = pygame.time.Clock()
 game_over = False
@@ -25,8 +36,13 @@ while not game_over:
     dt = clock.tick(50)
     screen.fill((0,0,0))
 
+    for b in bricks:
+        screen.blit(brick, b)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+    pygame.display.update()
 
 pygame.quit()
