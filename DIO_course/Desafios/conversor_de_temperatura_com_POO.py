7de03e3be70facc8224bdea06bb0cddcf5a9f753
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-05 22:46:01 -0300

    Create conversor_de_temperatura_com_POO.py

---
+++ DIO_course/Desafios/conversor_de_temperatura_com_POO.py
@@ -0,0 +1,17 @@
+    #TODO: Crie uma classe para converter temperaturas de Celsius para Fahrenheit e um método que realiza o cálculo de conversão:
+    class conversorTemperatura:
+        def __init__(self, celsius):
+            self.celsius = celsius
+
+        def celsius_para_fahreneit(self):
+            return (9/5*celsius)+32
+
+
+    # Entrada do usuário
+    celsius = float(input())
+
+    # TODO: Crie uma instância do conversor:
+    conversor = conversorTemperatura(celsius)
+
+    fahrenheit = conversorTemperatura.celsius_para_fahreneit(celsius)
+    print(fahrenheit)
