b198def33a07827fcdd8207d743374f609a6d5af
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-07 13:54:21 -0300

    Create h_delete_an_object.py

---
+++ w3_schools/class_and_objects/h_delete_an_object.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name,lname, age):
+        self.name = name
+        self.age = age
+        self.lname = lname
+
+    def all_name(self):
+        print(f'The name is {self.name} and the age is {self.age}.')
+
+p = Person('Joao', 'Oliveira', 12)
+
+del p
+p.all_name()
