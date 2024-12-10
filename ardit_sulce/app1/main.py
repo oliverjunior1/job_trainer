9d5266b58a5925daef6b62ddcc218d0f1d58d7f7
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-10 17:06:02 -0300

    Update main.py

--- ardit_sulce/app1/main.py
+++ ardit_sulce/app1/main.py
@@ -1,8 +1,16 @@
-user_prompt = 'Enter a todo:'
 
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.capitalize())
-    todos.append(todo)
+    user_action = input('Type add or show: ')
+
+    match user_action:
+        case 'add':
+            todo = input('Enter a todo:')
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+
+print('Bye!')
