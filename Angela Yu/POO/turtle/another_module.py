9dc2f07f297be9c5c1e078c8d316e606e80dd391
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-09 22:40:07 -0300

    Update another_module.py

--- Angela Yu/POO/turtle/another_module.py
+++ Angela Yu/POO/turtle/another_module.py
@@ -1,15 +1,28 @@
 # import another_module
 # print(another_module.another_variable)
+#
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# timmy.color('blueViolet')
+# timmy.backward(100)
+# #timmy.goto(500,0)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
+import prettytable
 
-timmy = Turtle()
-print(timmy)
-timmy.shape('turtle')
-timmy.color('blueViolet')
-timmy.backward(100)
-#timmy.goto(500,0)
+from prettytable import PrettyTable, from_csv
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+
+
+
+with open('Pokemon.csv') as fp:
+    mytable = from_csv(fp)
+
+print(mytable)
