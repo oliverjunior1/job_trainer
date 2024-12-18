fcd1f9562be874d6accf57ffff52ef9ac274ee7c
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-18 19:31:19 -0300

    Create merging_zip.py

---
+++ Trainer_in_job/exercises_zip/merging_zip.py
@@ -0,0 +1,8 @@
+lst1=[19542209, 4887871, 1420491, 626299, 1805832, 39865590]
+lst2=["New York", "Alabama", "Hawaii", "Vermont", "West Virginia",  "California"]
+
+#Type your answer here.
+
+lst3=zip(lst1, lst2)
+
+print(list(lst3))
