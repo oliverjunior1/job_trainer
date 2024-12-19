b36fca4493efd07363568c954a1b8810c7ac06cd
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-19 16:42:12 -0300

    Create merging_a_list_and_a_range.py

---
+++ Trainer_in_job/exercises_zip/merging_a_list_and_a_range.py
@@ -0,0 +1,12 @@
+lst1=["Energy", "Agriculture", "Industry", "Technology", "Finance", "Forestry", "Transport"]
+
+
+#Type your answer here.
+
+rng1= range(1,9)
+lst=list(zip(lst1, rng1))
+
+
+print(lst)
+
+
