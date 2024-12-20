1a4a9c0ca2f791afcf208a0cd3bf4d468c565f46
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-20 17:18:24 -0300

    Update main.py

--- ardit_sulce/app1/main.py
+++ ardit_sulce/app1/main.py
@@ -1,8 +1,10 @@
 while True:
+    # Get user input and strip space chars from it
     user_action = input('Type add, complete,show or edit: ')
     user_action = user_action.strip()
 
     match user_action:
+        # Check if user action is 'add'
         case 'add':
             todo = input('Enter a todo:')+'\n'
 
@@ -11,7 +13,7 @@ while True:
             file.close()
 
             todos.append(todo)
-
+            
             file = open('todos.txt', 'w')
             file.writelines(todos)
             file.close()
@@ -19,9 +21,22 @@ while True:
             file = open('todos.txt', 'r')
             todos = file.readlines()
             file.close()
-            for index, item in enumerate(todos):
-                print(f'{index}-{item}')
-            print(f'Lenght is {index + 1}')
+            # for index, item in enumerate(todos):
+            #     print(f'{index}-{item}')
+            # print(f'Lenght is {index + 1}')
+
+            new_todos = []
+
+            for item in todos:
+                new_item = item.strip('\n')
+                new_todos.append(new_item)
+            #print(todos)
+
+            #new_todos =[item.strip('\n') for item in todos]
+
+            for index, item in enumerate(new_todos):
+                row = f'{index + 1}-{item}'
+                print(row)
         case 'edit':
             number = int(input('Number of the todo to edit:'))
             number = number - 1
