bc5a4960ec84622fc0dd9329fc58eb81c86c198a
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-22 16:49:57 -0300

    Update first_reduce_code.py

--- geek_to_geek/Python fundamentals/with_statement/first_reduce_code.py
+++ geek_to_geek/Python fundamentals/with_statement/first_reduce_code.py
@@ -1,1 +1,23 @@
-file = open('file_path', 'r')
+# # 1) without using with statement
+# file = open('file_path', 'w')
+# file.write('hello world!')
+# file.close()
+#
+# # 2) without using with statement
+# file = open('file_path', 'w')
+# try:
+#     file.write('hello world')
+# finally:
+#     file.close()
+#
+# x = open('file_path', 'r')
+#
+# print(x.read())
+
+# using with statement
+with open('file_path', 'w') as file:
+    file.write('Jesus is the way!')
+
+x = open('file_path', 'r')
+
+print(x.read())
