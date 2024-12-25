aed12dd67404ce866a7d090dbd857949e7c7ac39
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-25 19:07:48 -0300

    Update function_to_process_a_list_1.py

--- ardit_sulce/day_11/function_to_process_a_list_1.py
+++ ardit_sulce/day_11/function_to_process_a_list_1.py
@@ -1,9 +1,8 @@
 def get_max():
     grades =[9.6, 9.2, 9.7]
     grades.sort(reverse=True)
-    return grades[0]
+    #change the function to return:
+    return f"Max: {grades[0]}, Min: {grades[-1]}"
 
-
-
-# calculate the maximum value of the grades list
+# show the result
 print(get_max())
