050aaaf2bae03d9ed7b7c1849a5d3240ae5775ab
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-28 17:54:11 -0300

    Update testing_1.py

--- ardit_sulce/test/testing_1.py
+++ ardit_sulce/test/testing_1.py
@@ -1,11 +1,11 @@
-def greeti(message):
-    new_message = message.capitalize()
-    print('Hey hey')
-    return new_message
+def strength(password):
+    if len(password) < 8:
+        print('Weak Password')
+    if not any(char.isupper() for char in password):
+        return 'Weak Password'
+    if not any(char.isdigit() for char in password):
+        return 'Weak Password'
+    return 'Strong Password'
 
-greet_typed = input("Type your greeting:")
-greeting = greeti(greet_typed)
-
-print(greeting)
-
+print(strength(input('Enter the password: ')))
 
