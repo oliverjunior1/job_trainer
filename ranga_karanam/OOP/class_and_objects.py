13e79c399b272f1a6866d94f22f7f62e6ef840f6
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-31 18:15:26 -0300

    Create class_and_objects.py

---
+++ ranga_karanam/OOP/class_and_objects.py
@@ -0,0 +1,12 @@
+# Creating the planet Class
+class Planet:
+    pass
+
+earth = Planet()
+venus = Planet()
+
+earth.name = 'The Earth'
+venus.name = 'The Venus'
+
+print(earth.name)
+print(venus.name)
