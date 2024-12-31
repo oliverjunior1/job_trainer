fc08de9454974c6aec83332f7572bc2b5603de73
oliverjunior1 <oliver-junior@hotmail.com>
2024-12-31 17:56:10 -0300

    Create creating_the_motorbike_class.py

---
+++ ranga_karanam/OOP/creating_the_motorbike_class.py
@@ -0,0 +1,12 @@
+class Motorbike:
+    pass
+
+honda = Motorbike()
+ducati = Motorbike()
+honda.name = 'Honda'
+honda.speed = 150
+ducati.name = 'Ducati'
+ducati.speed = 130
+
+print(honda.speed)
+print(ducati.speed)
