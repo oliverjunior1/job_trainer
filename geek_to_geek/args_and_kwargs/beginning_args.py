63459e096938ff1dbbefec83b79f461eab2b9f62
oliverjunior1 <oliver-junior@hotmail.com>
2025-01-09 18:03:33 -0300

    Update beginning_args.py

--- geek_to_geek/args_and_kwargs/beginning_args.py
+++ geek_to_geek/args_and_kwargs/beginning_args.py
@@ -1,6 +1,12 @@
-def myFun(*argv):
-    for arg in argv:
-        print(arg)
+# def myFun(*argv):
+#     for arg in argv:
+#         print(arg)
+#
+#
+# myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
 
+def mult(*args):
+    for arg in args:
+        print(arg**2)
 
-myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+mult(1,2,3,4,5,6)
