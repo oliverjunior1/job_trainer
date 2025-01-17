bd9b66a9dd21a4089fdb7dd8b343aab2ba9874a3
oliverjunior1 <oliver-junior@hotmail.com>
2025-01-16 21:36:49 -0300

    Update change_the_name_of_a_window.py

--- geek_to_geek/pygame/beginning/change_the_name_of_a_window.py
+++ geek_to_geek/pygame/beginning/change_the_name_of_a_window.py
@@ -4,6 +4,8 @@ pygame.init()
 
 screen = pygame.display.set_mode((400,500))
 pygame.display.set_caption('GeekForGeek')
+Icon = pygame.image.load('plant.png')
+pygame.display.set_icon(Icon)
 
 running = True
 
