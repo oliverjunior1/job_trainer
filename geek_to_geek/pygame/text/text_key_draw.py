39d0cec3a9740c17dd2a9c6f938e3f8edfd7d98e
oliverjunior1 <oliver-junior@hotmail.com>
2025-01-31 08:15:04 -0300

    Update text_key_draw.py

--- geek_to_geek/pygame/text/text_key_draw.py
+++ geek_to_geek/pygame/text/text_key_draw.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((500,500))
 pygame.draw.rect(screen, (255,255,255),(70,280,350,30))
 
 font = pygame.font.SysFont('freesanbold.ttf', 50)
-font2 = pygame.font.SysFont('freesanbold.ttf', 20)
+font2 = pygame.font.SysFont('Lucida Console', 14)
 text = font.render('Jesus is the way,', True, (255,255,255))
 text2 = font2.render('type left and right on the keyboard to see all the message!', True, (100,150,100))
 text_rect2 = text2.get_rect()
@@ -29,6 +29,6 @@ while True:
                 text = font.render('and the life!', True, (100, 100, 100))
                 text_rect.bottom=(350)
             if event.key==pygame.K_RIGHT:
-                text = font.render('is the truth,', True, (255, 0, 0))
+                text = font.render('the truth,', True, (255, 0, 0))
                 text_rect.center=(250,300)
     pygame.display.flip()
