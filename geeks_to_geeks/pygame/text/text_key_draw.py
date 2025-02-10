86046c1d0ae997452eec9adcec8fca7d2da41564
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-09 22:50:39 -0300

    Update text_key_draw.py

--- geeks_to_geeks/pygame/text/text_key_draw.py
+++ geeks_to_geeks/pygame/text/text_key_draw.py
@@ -28,7 +28,59 @@
 #     pygame.display.flip()
 #
 #---------------------------------Pandas--------------------------
-import pandas as pd
-import numpy as np
-
-ser = pd.Series()
+# import pandas as pd
+# import numpy as np
+#
+# x = np.array([1,7,9])
+#
+# y = pd.Series(x)
+#
+# print(y)
+#-------------------------------tkinter--------------------------
+#
+# import tkinter as tk
+# import tkinter as ttk
+#
+# from click import command
+#
+# root = tk.Tk()
+#
+# def change():
+#     label2 = tk.Label(root, text='I love you Jesus').pack()
+#
+# label = tk.Label(root, text='Jesus is the way').pack()
+# button = tk.Button(root, text='Click here').pack()
+# button2 = tk.Button(root, text='Click here 2',command=change).pack()
+# input = tk.Entry(root, text=' ').pack()
+#
+# root.mainloop()
+#---------------------------matplotlib--------------------------------------
+#
+# import matplotlib.pyplot as plt
+#
+# x = [1,5,7,10,6,15,25,16]
+#
+# plt.plot(x)
+#
+# plt.show()
+#---------------------------lambda----------------------------------------
+# x = lambda a, b, c: a**b//c
+#
+# print(x(5,2,5))
+#---------------------------class-----------------------------------------
+# class Name:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __str__(self):
+#         return f'The name is {self.name} Oliveira and the age is {self.age} years old.'
+#
+# name = Name('Joao', 12)
+# name1 = Name('Alyne', 38)
+# name2 = Name('Mariane', 3)
+#
+# print(name)
+# print(name1)
+# print(name2)
+#------------------------------
