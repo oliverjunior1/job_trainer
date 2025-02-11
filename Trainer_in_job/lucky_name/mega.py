c6b431d40e0b28d21d0fc5814a5d146fda169096
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-10 22:39:25 -0300

    Update mega.py

--- Trainer_in_job/lucky_name/mega.py
+++ Trainer_in_job/lucky_name/mega.py
@@ -4,8 +4,14 @@ def sorteio_mega_sena():
     numeros_sorteados = random.sample(range(1, 61), 6)
     return sorted(numeros_sorteados)
 
+def sorteio_lotofacil():
+    numeros_sorteados2 = random.sample(range(1, 25), 15)
+    return sorted(numeros_sorteados2)
+
 # Realizando o sorteio
 resultado = sorteio_mega_sena()
-print(f"Números sorteados:{resultado}")
+resultado2 = sorteio_lotofacil()
+print(f"Números sorteados megasena:{resultado}")
+print(f"Números sorteados lotofacil:{resultado2}")
 
 
