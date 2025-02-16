b0f37a251f3d408380a1e2f3e658647b4c272a6c
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-15 21:31:44 -0300

    Update Treinamentos.py

--- Trainer_in_job/Treinamentos.py
+++ Trainer_in_job/Treinamentos.py
@@ -42,36 +42,61 @@
 # button.pack()
 # root.mainloop()
 #--------------------------------Pygame----------------------------------------
-import pygame
-import sys
+# import pygame
+# import sys
+#
+# pygame.font.init()
+# pygame.font.get_fonts()
+#
+# screen = pygame.display.set_mode((500,500))
+#
+# # draw
+# draw = pygame.draw.rect(screen, 'lightblue', (40,40,30,30))
+#
+# # text
+# font = pygame.font.SysFont('Times New Roman', 25)
+# text = font.render('Jesus is in the door and knock!', True, 'yellow')
+# text_rect = text.get_rect()
+# text_rect.center=(250,250)
+#
+# # finish
+# while True:
+#     screen.blit(text, text_rect)
+#     for event in pygame.event.get():
+#         if event.type==pygame.QUIT:
+#             sys.exit()
+#             pygame.quit()
+#         if event.type==pygame.KEYDOWN:
+#             if event.key==pygame.K_1:
+#                 screen.fill('pink')
+#                 draw = pygame.draw.rect(screen, 'red', (200,200,30,60))
+#             if event.key == pygame.K_2:
+#                 screen.fill('light green')
+#                 draw = pygame.draw.rect(screen, 'orange', (100, 100, 30, 60))
+#
+#     pygame.display.flip()
+#----------------------------matplotlib--------------------------------------------
+# import matplotlib.pyplot as plt
+#
+# x = [1,7,3,11,12,9,15]
+#
+# plt.plot(x)
+#
+# plt.show()
+#------------------------------pandas---------------------------------------------
+# import pandas as pd
+#
+# x = pd.array([5,1,7,3])
+# y =pd.Series(x)
+#
+# print(y)
+#------------------------------pandas-matplotlib-----------------------------------
+import matplotlib.pyplot as plt
+import pandas as pd
 
-pygame.font.init()
-pygame.font.get_fonts()
+x = pd.array([1,7,5,11])
+y = pd.Series(x)
 
-screen = pygame.display.set_mode((500,500))
+plt.plot(y)
 
-# draw
-draw = pygame.draw.rect(screen, 'lightblue', (40,40,30,30))
-
-# text
-font = pygame.font.SysFont('Times New Roman', 25)
-text = font.render('Jesus is in the door and knock!', True, 'yellow')
-text_rect = text.get_rect()
-text_rect.center=(250,250)
-
-# finish
-while True:
-    screen.blit(text, text_rect)
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            sys.exit()
-            pygame.quit()
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_1:
-                screen.fill('pink')
-                draw = pygame.draw.rect(screen, 'red', (200,200,30,60))
-            if event.key == pygame.K_2:
-                screen.fill('light green')
-                draw = pygame.draw.rect(screen, 'orange', (100, 100, 30, 60))
-
-    pygame.display.flip()
+plt.show()
