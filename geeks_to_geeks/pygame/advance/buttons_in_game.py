bcdfdcacbc7a229935f6dea5082825ba332a285a
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-17 21:17:46 -0300

    Update buttons_in_game.py

--- geeks_to_geeks/pygame/advance/buttons_in_game.py
+++ geeks_to_geeks/pygame/advance/buttons_in_game.py
@@ -1,31 +1,27 @@
 import pygame
 import sys
 
-
-pygame.font.init()
-pygame.font.get_fonts()
+pygame.init()
 
 screen = pygame.display.set_mode((720,720))
-width = screen.get_width()
-height = screen.get_height()
+
 # text
-font = pygame.font.SysFont('Times New Roman', 25)
+font = pygame.font.SysFont('Times New Roman',25)
 text = font.render('quit', True, 'white')
 text_rect = text.get_rect()
 text_rect.center=(360,360)
 
-# design button
-rect = pygame.draw.rect(screen, 'red', (320,340,80,40))
+# draw
+draw = pygame.draw.rect(screen, 'red', (315,340,100,35))
 
 while True:
     screen.blit(text, text_rect)
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             sys.exit()
             pygame.quit()
         if event.type==pygame.MOUSEBUTTONDOWN:
-            if width/2 or height/2:
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
                 pygame.quit()
-
-
+        mouse = pygame.mouse.get_pos()
     pygame.display.flip()
