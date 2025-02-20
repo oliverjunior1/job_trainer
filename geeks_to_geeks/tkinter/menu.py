158fc5bf9e13b9f065059e41edc6433070afb239
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-20 18:41:08 -0300

    Update menu.py

--- geeks_to_geeks/tkinter/menu.py
+++ geeks_to_geeks/tkinter/menu.py
@@ -1,14 +1,15 @@
-from tkinter import *
-root = Tk()
-menu =Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-menu.add_cascade(label='File', menu=filemenu)
-filemenu.add_command(label='New')
-filemenu.add_command(label='Open...')
-filemenu.add_separator()
-filemenu.add_command(label='Exit', command=root.quit)
-helpmenu = Menu(menu)
-menu.add_cascade(label='Help', menu=helpmenu)
-helpmenu.add_command(label='About')
-mainloop()
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((500,500))
+
+# 
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit()
+            pygame.quit()
+    pygame.display.update()
