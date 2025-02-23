bd634dba5e28bf56c7e11402475c94650db2893c
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-23 12:25:06 -0300

    Update Treinamentos.py

--- Trainer_in_job/Treinamentos.py
+++ Trainer_in_job/Treinamentos.py
@@ -1,159 +1,54 @@
-#---------------------------lambda------------------------------------------
-# x = lambda a, b: a**b
+#---------------------------lambda---------------------------------------
+# x = lambda a, b: a **b
 #
-# print(x(5,4))
-#---------------------------List_Comprehension------------------------------
-# x = [1,2,3,4,5,6,7,8]
+# print(x(5,3))
+#---------------------------list_comprehension---------------------------
+# list = [1,2,3,4,5,6,7,8,9,10]
 #
-# x_2 = [a**2 for a in x]
+# list_2 = [x**3 for x in list]
 #
-# print(x_2)
-#---------------------------------------------------------------------------
-# fruits = ['apple', 'melon', 'pineapple', 'lemon']
-#
-# fruits_a = [x for x in fruits if 'a' in x]
-#
-# print(fruits_a)
-#----------------------------class------------------------------------------
-# class Bless:
-#     def __init__(self, name, age, life ):
-#         self.name = name
-#         self.age = age
-#         self.life = life
-#
-#     def __str__(self):
-#         return f'God, bless {self.name} who has {self.age}, the {self.life} area, in the name of Jesus!'
-#
-# bless = Bless('Joao Pedro', 12, 'Health')
-# bless2 = Bless('Mariane Vitoria', 3, 'Health')
-#
-# print(bless)
-# print(bless2)
-#--------------------------------Tkinter---------------------------------------
-# import tkinter as tk
-#
-# root = tk.Tk()
-# root.geometry('500x500')
-# label = tk.Label(root, text='Jesus is the light of the world!')
-# label.pack()
-# def text():
-#     label = tk.Label(root, text='How I love You my Lord Jesus!').pack()
-# button = tk.Button(root, text='CLick here', command=text)
-# button.pack()
-# root.mainloop()
-#--------------------------------Pygame----------------------------------------
-# import pygame
-# import sys
-#
-# pygame.font.init()
-# pygame.font.get_fonts()
-#
-# screen = pygame.display.set_mode((500,500))
-#
-# # draw
-# draw = pygame.draw.rect(screen, 'lightblue', (40,40,30,30))
-#
-# # text
-# font = pygame.font.SysFont('Times New Roman', 25)
-# text = font.render('Jesus is in the door and knock!', True, 'yellow')
-# text_rect = text.get_rect()
-# text_rect.center=(250,250)
-#
-# # finish
-# while True:
-#     screen.blit(text, text_rect)
-#     for event in pygame.event.get():
-#         if event.type==pygame.QUIT:
-#             sys.exit()
-#             pygame.quit()
-#         if event.type==pygame.KEYDOWN:
-#             if event.key==pygame.K_1:
-#                 screen.fill('pink')
-#                 draw = pygame.draw.rect(screen, 'red', (200,200,30,60))
-#             if event.key == pygame.K_2:
-#                 screen.fill('light green')
-#                 draw = pygame.draw.rect(screen, 'orange', (100, 100, 30, 60))
-#
-#     pygame.display.flip()
-#----------------------------matplotlib--------------------------------------------
-# import matplotlib.pyplot as plt
-#
-# x = [1,7,3,11,12,9,15]
-#
-# plt.plot(x)
-#
-# plt.show()
-#------------------------------pandas---------------------------------------------
-# import pandas as pd
-#
-# x = pd.array([5,1,7,3])
-# y =pd.Series(x)
-#
-# print(y)
-#------------------------------pandas-matplotlib-----------------------------------
-# import matplotlib.pyplot as plt
-# import pandas as pd
-#
-# x = pd.array([1,7,5,11])
-# y = pd.Series(x)
-#
-# plt.plot(y)
-#
-# plt.show()
-#----------------------------------pygames_new------------------------------------
-# list
-#
-# mylist = [1,2,3,4,5]
-# print(type(mylist))
-# print(mylist)
-#
-# # tupple
-# mytupple = (1,2,3,4,5,6)
-# print(type(mytupple))
-# print(mytupple)
-#
-# # set
-# myset = {1,2,3,4,5}
-# print(type(myset))
-# print(myset)
-#
-# # dict
-# mydict = {'name':'Alyne', 'age':18, 'name': 'Joaquim', 'age':42 }
-# print(type(mydict))
-# print(mydict)
-#---------------------------------mega_sena------------------------------------------
+# print(list_2)
+#-----------------------------try_except----------------------------------
+# try:
+#     x = 20/0
+#     print(x)
+# except:
+#     print('Not divisible for zero!')
+#-----------------------------mega---------------------------------------
 # import random
-#
 # def lucky():
 #     x = random.sample(range(1,61),6)
 #     return sorted(x)
-#
 # y = lucky()
-#
 # print(y)
+#---------------------------pygame---------------------------------------
+import pygame
+import sys
 
-#----------------------------------tkinter------------------------------------------
-import tkinter as tk
-import random
+from narwhals import scan_csv
 
-root = tk.Tk()
-def mega():
-    x = random.sample(range(1,61), 6)
-    label.config(text=f'{sorted(x)}')
-    return label.pack()
-def lotofacil():
-    y = random.sample(range(1,26),15)
-    label.config(text=f'{sorted(y)}')
-    return label.pack()
+pygame.init()
+drawer = (40,40,30,30)
+screen = pygame.display.set_mode((500,500))
 
+# draw
+draw = pygame.draw.rect(screen, 'red', drawer)
 
-root.geometry(('500x600'))
-label = tk.Label(text='Jesus is the light of the world!')
-button = tk.Button(text='click here to show lotofacil:', command=lotofacil)
-button2 = tk.Button(text='click here to show megasena:', command=mega)
-button2.pack()
-label.pack()
-button.pack()
-
-
-root.mainloop()
+# text
+font = pygame.font.SysFont('Times New Roman', 25)
+text = font.render('Jesus is the light of the World!', True, 'blue')
+text_rect = text.get_rect()
+text_rect.center=(250,250)
+# ending
+while True:
+    screen.blit(text, text_rect)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit()
+            pygame.quit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_UP:
+                draw = pygame.draw.rect(screen, 'green', drawer)
+            elif event.key==pygame.K_DOWN:
+                draw = pygame.draw.rect(screen, 'pink', drawer)
+    pygame.display.flip()
