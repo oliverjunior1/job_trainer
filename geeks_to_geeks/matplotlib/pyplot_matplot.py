ccc00a8f0624ba7d0bb29a55bf38910ceb483e13
oliverjunior1 <oliver-junior@hotmail.com>
2025-02-24 21:31:18 -0300

    Update pyplot_matplot.py

--- geeks_to_geeks/matplotlib/pyplot_matplot.py
+++ geeks_to_geeks/matplotlib/pyplot_matplot.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-x = [1,7,9]
-y = [15,20,25]
+x = [1,5,3,7,9,12]
+y = [9,10,11,12,13,14]
 
 fig, ax = plt.subplots()
 ax.plot(x, y, marker='o', label='Data Points')
 
 ax.set_title('Basic Components of Matplotlib Figure')
-ax.set_xlabel('X-Axis')
-ax.set_ylabel('Y-Axis')
+ax.set_xlabel('X-label')
+ax.set_ylabel('X-label')
 
 plt.show()
