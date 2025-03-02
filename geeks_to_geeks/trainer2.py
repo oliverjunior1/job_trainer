c3c817b32c5c8332cf9f1f63cbc038cde18d2c19
oliverjunior1 <oliver-junior@hotmail.com>
2025-03-02 19:13:45 -0300

    Update trainer2.py

--- geeks_to_geeks/trainer2.py
+++ geeks_to_geeks/trainer2.py
@@ -1,8 +1,21 @@
+import tkinter as tk
 import random
 
-def lucky():
-    x = random.sample(range(1,25),15)
-    return sorted(x)
+root = tk.Tk()
+root.geometry(('1300x200'))
+def megasena():
+    x = random.sample(range(1,61), 6)
+    sorted(x)
+    return label.config(text=f'The numbers of megasena are: {sorted(x)}')
+def lotofacil():
+    x = random.sample(range(1,25), 15)
+    sorted(x)
+    return label.config(text=f'The numbers of megasena are: {sorted(x)}')
 
-y = lucky()
-print(y)
+label = tk.Label(root, text="João 14:13-14. Nessa passagem, Jesus diz que fará tudo o que for pedido em seu nome para que o Pai seja glorificado no Filho. A passagem também diz que aquele que crê em Jesus fará as obras que ele faz e fará ainda maiores do que essas")
+label.pack()
+button1 = tk.Button(root, text='Click here to megasena', command=megasena)
+button1.pack()
+button2 = tk.Button(root, text='Click here to megasena', command=lotofacil)
+button2.pack()
+tk.mainloop()
