7bb438629fde3dbcf732568e84b2dbe0725a124b
oliverjunior1 <oliver-junior@hotmail.com>
2025-03-03 12:29:52 -0300

    Update trainer2.py

--- geeks_to_geeks/trainer2.py
+++ geeks_to_geeks/trainer2.py
@@ -1,5 +1,9 @@
-try:
-    x = 10/0
-    print(x)
-except:
-    print("Don't divisible by zero")
+# try:
+#     x = 10/0
+#     print(x)
+# except:
+#     print("Don't divisible by zero")
+#
+# x = lambda a, b: a **b
+#
+# print(x(4,4))
