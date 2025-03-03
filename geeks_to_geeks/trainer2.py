de1ef97713d9b12b5119c328fd9c42ca3e9ca850
oliverjunior1 <oliver-junior@hotmail.com>
2025-03-03 12:54:32 -0300

    Update trainer2.py

--- geeks_to_geeks/trainer2.py
+++ geeks_to_geeks/trainer2.py
@@ -7,3 +7,10 @@
 # x = lambda a, b: a **b
 #
 # print(x(4,4))
+import matplotlib.pyplot as plt
+
+x = [35,25,25,15]
+y = ['Python', 'Javascript', 'Java', 'C#']
+
+plt.pie(x, labels=y, autopct=f'%0.0f%%')
+plt.show()
