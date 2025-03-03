221e9b7987b125c98292e89430a4e2f77e625d67
oliverjunior1 <oliver-junior@hotmail.com>
2025-03-03 20:19:17 -0300

    Create b_pandas_series.py

---
+++ geeks_to_geeks/pandas_trainer/b_pandas_series.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+#
+# x = [1,2,3,4,5]
+#
+# y = pd.Series(x)
+# print(y)
+
+# import pandas as pd
+#
+# x = ['J', 'e', 's', 'u', 's', ' ','é',' ', 'o', 'r', 'e', 'i' ]
+#
+# y = pd.Series(x)
+# print(y[:5])
