99a22f48b520b6dcaddc1aec9e9f0b7ca4bdbefa
oliverjunior1 <oliver-junior@hotmail.com>
2025-03-05 20:08:23 -0300

    Create i_combo_box.py

---
+++ geeks_to_geeks/tkinter_trainer/i_combo_box.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+
+def select(event):
+    selected_item = combo_box.get()
+    label.config(text='Selected Item'+ selected_item)
+
+root = tk.Tk()
+root.title('Combobox Example')
+
+label = tk.Label(root, text='Selected Item: ')
+label.pack(pady=10)
+
+combo_box = ttk.Combobox(root, values=['Option 1', 'Option 2', 'Option 3'])
+combo_box.pack(pady=5)
+
+combo_box.set('Option 1')
+
+combo_box.bind('<<ComboboxSelected>>', select)
+
+root.mainloop()
